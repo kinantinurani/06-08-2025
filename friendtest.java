696422f8ac869ed35d4eafa32138df44f2c8b555
kinantinurani <kinanti_nurani_34rpl@student.smktelkom-mlg.sch.id>
2025-08-06 15:01:41 +0700

    Add files via upload

---
+++ friendtest.java
@@ -0,0 +1,21 @@
+//Driver Class
+public class friendtest {
+    public static void main(String[] args) {
+        //membuat object
+        friend salma = new friend("salma",
+        "pondok pesantren darrul ukkhuwah", 'p', 15);
+       salma.cetak();
+        friend kia = new friend("kia",
+        "pondok pesantren tebu ireng", 'p', 15);
+        kia.cetak();
+       friend jasmine = new friend("jasmine",
+        "smp 01 lawang", 'p', 15);
+       jasmine.cetak();
+       friend abel = new friend("abel",
+        "smp 16 malang", 'p', 15);
+       abel.cetak();
+        friend anggun = new friend("anggun",
+        "MTsN 2 malang", 'p', 15);
+       anggun.cetak();
+    }}
+
